80a5413f34e0db58f068e1de5ade6788df6652e4
thiagoavelino <thiago@thiagoavelino.com.br>
2015-06-21 20:59:32 -0300

    Chapter 3 exercises

---
+++ VisualStudio_C/Chapter3/AbstractClasses.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualStudio_C.Chapter3 {
+    // an abstract method inside a class
+    public abstract class Student {
+        public abstract void outputDetails();
+    }
+    public class CollegeStudent : Student {
+        public string firstName;
+        public string lastName;
+        public string major;
+        public double GPA;
+        public override void outputDetails() {
+            Console.WriteLine("Student " + firstName + " " + lastName +
+            " enrolled in " + major + " is has a GPA of " + GPA);
+        }
+    }
+    class AbstractClasses {
+        static void MainAbstractClasses(string[] args) {
+            var collegeStudent = new CollegeStudent();
+            collegeStudent.firstName = "Thiago";
+            collegeStudent.lastName = "Avelino";
+            collegeStudent.major = "Computer Engineer";
+            collegeStudent.GPA = 12.4;
+            collegeStudent.outputDetails();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ VisualStudio_C/Chapter3/ExtendedMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualStudio_C.Chapter3 {
+    public static class MyExtendedMethods {
+        public static int square(this int num) {
+            int result = 0;
+            result = num * num;
+            return result;
+        }
+    }
+    class ExtendedMethods {
+        static void Main(string[] args) {
+            var myNum = 3;
+            myNum.square();
+            Console.WriteLine(myNum + 3);
+            Console.ReadKey();
+        }
+    }
+}
