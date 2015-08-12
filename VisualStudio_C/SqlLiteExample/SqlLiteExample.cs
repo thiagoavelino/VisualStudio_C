f405e548ad8fdd4d1757d1db583980330a8ad525
thiagoavelino <thiago@thiagoavelino.com.br>
2015-08-11 22:15:22 -0300

    Sqlite example for tomorrow

--- VisualStudio_C/SqlLiteExample/SqlLiteExample.cs
+++ VisualStudio_C/SqlLiteExample/SqlLiteExample.cs
@@ -11,27 +11,38 @@ namespace VisualStudio_C.SqlLiteExample {
     public class SqlLiteExample {
         public static void Main(string[] args) {
 
-            string path = "MyTestsFile2.txt";
+            //string path = "MyTestsFile2.txt";
 
             // Create a file to write to. 
-            using(StreamWriter sw = File.CreateText(path)) {
-                for(int i = 0; i < 1000; i++) {
-                    sw.WriteLine("update test set someValue = 'test{0}' where id = {0}", i);
-                }
-            }
-            Console.ReadKey();
+            //using(StreamWriter sw = File.CreateText(path)) {
+            //    for(int i = 0; i < 1000; i++) {
+            //        sw.WriteLine("update test set someValue = 'test{0}' where id = {0};", i);
+            //    }
+            //}
+            
 
             var listOfCommands = new List<String>();
 
-            using(StreamReader sr = File.OpenText(path)) {
-                string s = "";
-                while((s = sr.ReadLine()) != null) {
-                    listOfCommands.Add(s);
-                }
+            //using(StreamReader sr = File.OpenText(path)) {
+            //    string s = "";
+            //    while((s = sr.ReadLine()) != null) {
+            //        listOfCommands.Add(s);
+            //    }
+            //}
+            //ExecuteBatchSqlNoParameters("CREATE INDEX i on test(id)");
+
+            //Console.ReadKey();
+
+            var stringWithCommands = new StringBuilder();
+            for(int i = 0; i < 100001; i++) {
+                //stringWithCommands.Append(String.Format("update test set someValue = 'test{0}' where id = {0};"
+                //                            , i));
+                stringWithCommands.Append(String.Format("insert into test (id, someValue) values({0},'test');", i));
             }
+
             var initial = DateTime.Now;
             Console.WriteLine("Intial Time: " + initial);
-            ExecuteBatchSqlNoParameters(listOfCommands);
+            ExecuteBatchSqlNoParameters(stringWithCommands.ToString());
             var final = DateTime.Now;
             Console.WriteLine("Final Time: " + final);
             Console.ReadKey();
@@ -64,23 +75,38 @@ namespace VisualStudio_C.SqlLiteExample {
             //Console.ReadKey();
         }
 
-        public static void ExecuteBatchSqlNoParameters(List<string> updateSqlList) {
-            var connection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+        public static void ExecuteBatchSqlNoParameters(string commandText) {
+                var connection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+                connection.Open();
+                using(DbCommand command = connection.CreateCommand())
+                using(DbTransaction transaction = connection.BeginTransaction()) {
+                    command.CommandText = commandText;
+                    command.ExecuteNonQuery();
+                    transaction.Commit();
+                }
+                connection.Close();
+                connection.Dispose();
 
-            connection.Open();
 
-            using(DbCommand command = connection.CreateCommand())
-            using(DbTransaction transaction = connection.BeginTransaction()) {
-                foreach(var updateSqlText in updateSqlList) {
+        }
+
+        public static void ExecuteBatchSqlNoParametersParalle(List<string> updateSqlList) {
+            
+
+            Parallel.ForEach(updateSqlList, updateSqlText => {
+                var connection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+                connection.Open();
+                using(DbCommand command = connection.CreateCommand())
+                using(DbTransaction transaction = connection.BeginTransaction()) {
                     command.CommandText = updateSqlText;
                     command.ExecuteNonQuery();
+                    transaction.Commit();
                 }
+                connection.Close();
+                connection.Dispose();
+            });
 
-                transaction.Commit();
-            }
-
-            connection.Close();
-            connection.Dispose();
+            
         }
 
     }
