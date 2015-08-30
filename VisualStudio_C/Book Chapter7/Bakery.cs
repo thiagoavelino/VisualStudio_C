a93d604ac95a4b4a99883d2e071a3fcc73193846
thiagoavelino <thiago@thiagoavelino.com.br>
2015-08-30 09:15:49 -0300

    Consummer and Productor Problem

---
+++ VisualStudio_C/Book Chapter7/Bakery.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VisualStudio_C.Book_Chapter7 {
+    class Bakery {
+
+        Queue<Donut> donutTray = new Queue<Donut>();
+        public Donut GetDonut() {
+            lock(donutTray) {
+                while(donutTray.Count == 0) {
+                    Monitor.Wait(donutTray);
+                }
+                return donutTray.Dequeue();
+            }
+        }
+
+        public void ReffilTray(Donut[] freshDonuts) {
+            lock(donutTray) {
+                foreach(var donut in freshDonuts) {
+                    donutTray.Enqueue(donut);
+                }
+                Monitor.PulseAll(donutTray);
+            }
+        }
+    }
+
+
+
+    class Donut {
+        public Donut() {
+               
+        }
+    }
+}
---
+++ VisualStudio_C/Book Chapter7/Widget2D.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VisualStudio_C.Book_Chapter7 {
+    class Widget2D {
+        int x;
+        int y;
+        object _lock = new object();
+
+        public Widget2D(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void MoveBy(int deltaX, int deltaY) {
+            lock(_lock){
+                x += deltaX;
+                y += deltaY;
+            }
+        }
+
+        public void GetPos(out int x, out int y) {
+            lock(_lock){
+                x = this.x;
+                y = this.y;
+            } 
+        }
+    }
+}
